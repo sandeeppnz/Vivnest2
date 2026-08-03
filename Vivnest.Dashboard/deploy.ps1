c40e873866a01d980f194bf437bd87085652a030
sandeeppnz <sandeep.perera@gmail.com>
2026-08-04 00:11:58 +1200

    Update

--- Vivnest.Dashboard/deploy.ps1
+++
@@ -1,3 +0,0 @@
-TOKEN=$(az staticwebapp secrets list --name vivnest-dashboard --resource-group rg-vivnest-dev --query "properties.apiKey" -o tsv) 
-
-npx --yes @azure/static-web-apps-cli deploy ./dist --deployment-token "$TOKEN" --env production 2>&1
